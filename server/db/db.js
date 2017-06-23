3270ffad7dc151f9a5df31f063d4025d8016ac63
Amit Kumar <amit00272@hotmail.com>
2017-06-23 14:03:35 +0530

    initial commit

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,6 @@
 var mongoose=require('mongoose');
 
     mongoose.Promise=global.Promise;
-    mongoose.connect('mongodb://localhost:27017/TodoApp');
+    mongoose.connect('mongodb://amit00272:amit00272@ds135912.mlab.com:35912/todoapp||mongodb://localhost:27017/TodoApp');
 
 module.exports={mongoose};
